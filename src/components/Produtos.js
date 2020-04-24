00780a2b5ab1c3e83527e09836bb9b0d2dbc496b
Murilo Oliari Ribeiro <murilo.oliari@gmail.com>
2020-04-24 15:14:05 -0300

    branch3

--- src/components/Produtos.js
+++ src/components/Produtos.js
@@ -14,98 +14,115 @@ padding: 5px 10px;
 `
 export class Produtos extends React.Component {
     state = {
-        produtos: [{
-            id: Date.now(),
-            name: "Aerolitos do Chapolin",
-            value: 120342.01,
-            imageUrl: 'https://picsum.photos/200/200'
-        },
-        {
-            id: Date.now(),
-            name: "Meteorito Lovebackin Sevendays",
-            value: 10200.02,
-            imageUrl: 'https://picsum.photos/200/200'
-        },{
-            id: Date.now(),
-            name: "Meterorito de Grayskull",
-            value: 22134.23,
-            imageUrl: 'https://picsum.photos/200/200'
-        },{
-            id: Date.now(),
-            name: "Aerolito Filosofal",
-            value: 9234.23,
-            imageUrl: 'https://picsum.photos/200/200'
-        },
-        {
-            id: Date.now(),
-            name: "Aerolito Aerodactyl",
-            value: 6534.23,
-            imageUrl: 'https://picsum.photos/200/200'
-        },
-        {
-            id: Date.now(),
-            name: "Meterorito Checkmate",
-            value: 4534.65,
-            imageUrl: 'https://picsum.photos/200/200'
-        },
-        {
-            id: Date.now(),
-            name: "Aerolitos XPTO",
-            value: 2334.23,
-            imageUrl: 'https://picsum.photos/200/200'
-        },
-        {
-            id: Date.now(),
-            name: "Aerolitos XPTO",
-            value: 2334.23,
-            imageUrl: 'https://picsum.photos/200/200'
-        },
-        {
-            id: Date.now(),
-            name: "Aerolitos XPTO",
-            value: 2334.23,
-            imageUrl: 'https://picsum.photos/200/200'
-        },
-        {
-            id: Date.now(),
-            name: "Aerolitos XPTO",
-            value: 2334.23,
-            imageUrl: 'https://picsum.photos/200/200'
-        },
-        {
-            id: Date.now(),
-            name: "Aerolitos XPTO",
-            value: 2334.23,
-            imageUrl: 'https://picsum.photos/200/200'
-        }
-    ],
+        //produtos: []
     }
-
+//     {
+//         id: Date.now(),
+//         name: "Aerolitos do Chapolin",
+//         value: 120342.01,
+//         imageUrl: 'https://picsum.photos/200/200'
+//     },
+//     {
+//         id: Date.now(),
+//         name: "Meteorito Lovebackin Sevendays",
+//         value: 10200.02,
+//         imageUrl: 'https://picsum.photos/200/200'
+//     },{
+//         id: Date.now(),
+//         name: "Meterorito de Grayskull",
+//         value: 22134.23,
+//         imageUrl: 'https://picsum.photos/200/200'
+//     },{
+//         id: Date.now(),
+//         name: "Aerolito Filosofal",
+//         value: 9234.23,
+//         imageUrl: 'https://picsum.photos/200/200'
+//     },
+//     {
+//         id: Date.now(),
+//         name: "Aerolito Aerodactyl",
+//         value: 6534.23,
+//         imageUrl: 'https://picsum.photos/200/200'
+//     },
+//     {
+//         id: Date.now(),
+//         name: "Meterorito Checkmate",
+//         value: 4534.65,
+//         imageUrl: 'https://picsum.photos/200/200'
+//     },
+//     {
+//         id: Date.now(),
+//         name: "Aerolitos XPTO",
+//         value: 2334.23,
+//         imageUrl: 'https://picsum.photos/200/200'
+//     },
+//     {
+//         id: Date.now(),
+//         name: "Aerolitos XPTO",
+//         value: 2334.23,
+//         imageUrl: 'https://picsum.photos/200/200'
+//     },
+//     {
+//         id: Date.now(),
+//         name: "Aerolitos XPTO",
+//         value: 2334.23,
+//         imageUrl: 'https://picsum.photos/200/200'
+//     },
+//     {
+//         id: Date.now(),
+//         name: "Aerolitos XPTO",
+//         value: 2334.23,
+//         imageUrl: 'https://picsum.photos/200/200'
+//     },
+//     {
+//         id: Date.now(),
+//         name: "Aerolitos XPTO",
+//         value: 2334.23,
+//         imageUrl: 'https://picsum.photos/200/200'
+//     }
+// ],
+// popularProdutos = () => {
+//     let novaLista = this.state.produtos
+//     for(let i = 0; i < 7; i++) {
+//         const novoProduto = {
+//             id: Date.now(),
+//             name: `Produto - ${i+1}`,
+//             value: `R$ ${i + 10}`,
+//             imageUrl: `https://picsum.photos/200/200?id/${i}`
+//         }
+//         novaLista.push(novoProduto)
+//     }
+// }
     // popularProdutos = () => {
     //     let novaLista = this.state.produtos
-    //     for(let i = 0; i < 7; i++) {
+    //     for(let i = 0; this.state.produtos.length < 8; i++) {
     //         const novoProduto = {
-    //             id: Date.now(),
+    //             id: i+1,
     //             name: `Produto - ${i+1}`,
-    //             value: `R$ ${i + 10}`,
+    //             value: `R$ ${i * 3}`,
     //             imageUrl: `https://picsum.photos/200/200?id/${i}`
     //         }
     //         novaLista.push(novoProduto)
+    //         this.setState({produtos: novaLista})
     //     }
     // }
 
     render () {
-        const listaProdutos = this.state.produtos
+
+        //this.popularProdutos();
+        //const listaProdutos = this.state.produtos
+
+        // {listaProdutos.map((produto) => {
+        //     return  <div>
+        //                 <img src={produto.imageUrl} alt="imagem genérica" />
+        //                 <h4>{produto.name}</h4>
+        //                 <h4>{produto.value}</h4>
+        //                 <button onClick={() => this.props.aoAdicionar(produto.id, produto.name, produto.value, produto.imageUrl)}>add ao carrinho</button>
+        //             </div>
+        // })}
 
         return  <GridProdutos>
-                    {listaProdutos.map((produto) => {
-                        return  <div>
-                                    <img src={produto.imageUrl} alt="imagem genérica" />
-                                    <h4>{produto.name}</h4>
-                                    <h4>{produto.value}</h4>
-                                    <button>add ao carrinho</button>
-                                </div>
-                    })}
+    
                 </GridProdutos>
     }
 }
